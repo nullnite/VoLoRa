eecbb4b67027ec602c01db715179cbf9dc32ac49
Domantas Savickas <domantas.savickas@peplink.com>
2026-05-23 12:06:51 +0300

    nPM1300 LED blinking

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
  */
 
 #include <stdio.h>
+#include <zephyr/drivers/i2c.h>
 #include <zephyr/drivers/led.h>
 #include <zephyr/kernel.h>
 
@@ -17,15 +18,30 @@
 #define NPM1300_LED_RED   2
 
 static const struct device *leds = DEVICE_DT_GET(DT_NODELABEL(npm1300_leds));
+static const struct device *i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c1));
 
 int main(void) {
     bool led_state = true;
 
     printf("VoLoRa booted\n");
 
+    if (!device_is_ready(i2c_dev)) {
+        printf("I2C bus not ready\n");
+        return 0;
+    }
+    printf("I2C bus ready\n");
+
+    /* Scan for nPM1300 at address 0x6b */
+    uint8_t dummy;
+    int ret = i2c_read(i2c_dev, &dummy, 0, 0x6b);
+    printf("I2C probe 0x6b: %d\n", ret);
+
     if (!device_is_ready(leds)) {
         printf("nPM1300 LED device not ready\n");
-        return 0;
+        /* Keep running so we can see the output */
+        while (1) {
+            k_msleep(1000);
+        }
     }
 
     while (1) {
